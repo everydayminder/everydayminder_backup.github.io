7243fdd7d64b2fa4e6857ced2b2fa2e1e15bdb06
jaehyun kim <2jhyun@gmail.com>
2018-06-17 04:11:05 +0000

    New version - c4486116ca1ebf41e42409a28d4bc07601278c79 @Sun Jun 17 04:11:05 UTC 2018

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,52 +1,21 @@
----
-layout: null
----
-
-var store = [
-  {%- for c in site.collections -%}
-    {%- if forloop.last -%}
-      {%- assign l = true -%}
-    {%- endif -%}
-    {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
-    {%- for doc in docs -%}
-      {%- if doc.header.teaser -%}
-        {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
-      {%- else -%}
-        {%- assign teaser = site.teaser -%}
-      {%- endif -%}
-      {
-        "title": {{ doc.title | jsonify }},
-        "excerpt":
-          {%- if site.search_full_content == true -%}
-            {{ doc.content |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | jsonify }},
-          {%- else -%}
-            {{ doc.content |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-          {%- endif -%}
-        "categories": {{ doc.categories | jsonify }},
-        "tags": {{ doc.tags | jsonify }},
-        "url": {{ doc.url | absolute_url | jsonify }},
-        "teaser":
-          {%- if teaser contains "://" -%}
-            {{ teaser | jsonify }}
-          {%- else -%}
-            {{ teaser | absolute_url | jsonify }}
-          {%- endif -%}
-      }{%- unless forloop.last and l -%},{%- endunless -%}
-    {%- endfor -%}
-  {%- endfor -%}]
+var store = [{
+        "title": "20080709 Install Ubuntu 7.10 & VMware-tools",
+        "excerpt":"[C] Installiation of Ubuntu 7.10 flow downloaded the image file installation complete (without security packages) comments installed Ubuntu 8.04 (the latest version) failed to install other development packages on it version 8.04 seems NOT stable as 7.xx -&gt; let’s install version 7.10 instead it takes too long!! (about an hour)...","categories": ["system"],
+        "tags": ["tomcat"],
+        "url": "/system/20080709-install-ubuntu-710-vmware-tools/",
+        "teaser":null},{
+        "title": "Ubuntu에서 VIM이 재설치가 안될때",
+        "excerpt":"Ubuntu 7.10에서 기본적으로 설치되어 있는 Vim은 Tiny 버전이다.기타 필요한 VIM의 기능을 쓸 수가 없으므로, 재설치는 필수이다. sudo apt-get install vim그러나, 이 재설치가 제대로 안되는 경우가 있다.이럴 때, Ubuntu 소스의 repository를 제대로 업데이트 해줘야sudo apt-get update도 된다. 다음의 내용을 보완하자. /etc/apt/sources.list 파일에 아래의 내용을 추가(혹은 기존 내용은 모두 삭제 or 코멘트...","categories": ["tips"],
+        "tags": ["vim"],
+        "url": "/tips/install_vim_under_ubuntu/",
+        "teaser":null},{
+        "title": "HBase Installation (Standalone mode/local file system)",
+        "excerpt":"Requirements Java 1.5.x, preferably from Sun. Hadoop 0.16.x. This version of HBase will only run on Hadoop 0.16.x.. ssh must be installed and sshd must be running to use Hadoop’s scripts    to manage remote Hadoop daemons. HBase currently is a file handle hog. The usual default of 1024 on...","categories": ["system"],
+        "tags": ["hadoop"],
+        "url": "/system/hbase-installation-standalone-modelocal-file-system/",
+        "teaser":null},{
+        "title": "Hadoop Installation on Ubuntu Linux 7.10",
+        "excerpt":"Related articles by Michael G. Noll Running Hadoop On Ubuntu Linux (Single Node Cluster)http://wiki.apache.org/hadoop/Running_Hadoop_On_Ubuntu_Linux_%28Single-Node_Cluster%29 Running Hadoop On Ubuntu Linux (Multi Node Cluster)http://www.michael-noll.com/wiki/Running_Hadoop_On_Ubuntu_Linux_%28Multi-Node_Cluster%29 [Comments] I created a VMWare Team project file and added two instances of existing virtual machines. Each VMWare instance has a single node cluster. IP alias name should...","categories": ["system"],
+        "tags": ["hadoop"],
+        "url": "/system/hadoop-installation-on-ubuntu-linux-710/",
+        "teaser":null}]
